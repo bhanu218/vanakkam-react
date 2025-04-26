df823c89b6ff65aca2ac8f24c75c4822c5fec808
bhanu218 <bhanuteja.duddu@gmail.com>
2025-04-26 20:52:07 +0530

    small change in div h1 tag 1st child

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ const parent= React.createElement(
     [React.createElement(
         "div",{id:"child"},
             [
-                React.createElement("h1",{},"h1 tag line"),
-                React.createElement("h1",{},"h1 second line "),
-            ]
+                React.createElement("h1",{},"this is 1st child and 1st h1"),
+                React.createElement("h2",{},"this is 1st child and 2nd h2"),]
     
     ),
     React.createElement("div",{id:"child2"},
