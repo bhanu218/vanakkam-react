803134b2af0aba6b314065edab1063dfb987ff54
bhanu218 <bhanuteja.duddu@gmail.com>
2025-04-27 22:39:52 +0530

    react element and react component

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-const parent= React.createElement(
-    "div",{id:"parent"},
-    [React.createElement(
-        "div",{id:"child"},
-            [
-                React.createElement("h1",{},"this is 1st child and 1st h1"),
-                React.createElement("h2",{},"this is 1st child and 2nd h2"),]
-    
-    ),
-    React.createElement("div",{id:"child2"},
-        React.createElement("p",{},"2nd child of parent element")),
-]
+const userapi = "https://jsonplaceholder.typicode.com/users/1";
+
+const user = fetch(userapi)
+    .then(response => response.json())
+    .then(data =>{ console.log(data); return data; })
+    .then( collectedData => {console.log(collectedData.name)    ; return collectedData.name;})
+    .catch(error => console.error('Error fetching user:', error));
+
+
+const elem=<h1 id='elem
+'>this is element heading</h1>;
+   
+const App = () => (
+    <div className="app">
+        <h1>Hello {user}</h1>
+        {elem}
+        <p>This is a simple React application.</p>
+    </div>
 );
 
-const root =ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
-
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App></App>);
